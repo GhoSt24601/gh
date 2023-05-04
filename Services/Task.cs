7d243e8f5aa6ab0c269cea5c4d36e249a7890f2b
YourLifeForAiur <r320stwg@yandex.ru>
2023-05-04 18:04:25 +0500

    T1 complete

---
+++ Services/Task.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace Task.Services
+{
+    public partial class Task
+    {
+        static public void Number1()
+        {
+            StringBuilder input = new StringBuilder(Console.ReadLine());
+            StringBuilder newInput = new StringBuilder();
+            if (input.Length % 2 == 0)
+            {
+                for (int i = (input.Length / 2) - 1; i > -1; i--)
+                {
+                    newInput.Append(input[i]);
+                }
+                for (int i = input.Length - 1; i > (input.Length / 2) - 1; i--)
+                {
+                    newInput.Append(input[i]);
+                }
+            }
+            else
+            {
+                for (int i = input.Length - 1; i > -1; i--)
+                {
+                    newInput.Append(input[i]);
+                }
+                newInput.Append(input);
+            }
+            Console.WriteLine(newInput);
+        }
+    }
+}
